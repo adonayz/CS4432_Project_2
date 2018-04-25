d18e732f498914840d4bee952ff2935b3beda371
Adonay Z. Resom <adonayzenebe1000@gmail.com>
2018-04-25 14:35:49 -0400

    reversed initial changes (while keeping isSortedFlag and its methods)

--- src/simpledb/record/TableInfo.java
+++ src/simpledb/record/TableInfo.java
@@ -14,7 +14,6 @@ public class TableInfo {
    private int recordlen;
    private String tblname;
    private boolean isSorted = false; // CS4432-Project2: indicates if tables is sorted or not
-   private LinkedList<String> sortedColumns;
 
    /**
     * Creates a TableInfo object, given a table name
@@ -25,13 +24,15 @@ public class TableInfo {
     * @param schema the schema of the table's records
     */
    public TableInfo(String tblname, Schema schema) {
-      this(tblname, schema, new HashMap<>(), 0, false, new LinkedList<>());
+      this.schema = schema;
+      this.tblname = tblname;
+      offsets  = new HashMap<String,Integer>();
       int pos = 0;
       for (String fldname : schema.fields()) {
-         this.offsets.put(fldname, pos);
+         offsets.put(fldname, pos);
          pos += lengthInBytes(fldname);
       }
-      this.recordlen = pos;
+      recordlen = pos;
    }
    
    /**
@@ -45,29 +46,10 @@ public class TableInfo {
     * @param recordlen the already-calculated length of each record
     */
    public TableInfo(String tblname, Schema schema, Map<String,Integer> offsets, int recordlen) {
-      this(tblname, schema, new HashMap<>(), recordlen, false, new LinkedList<>());
-   }
-
-   // CS4432-Project2: The following constructor was added for this project
-   /**
-    * Creates a TableInfo object from the
-    * specified metadata.
-    * This constructor is used when the metadata
-    * is retrieved from the catalog.
-    * @param tblname the name of the table
-    * @param schema the schema of the table's records
-    * @param offsets the already-calculated offsets of the fields within a record
-    * @param recordlen the already-calculated length of each record
-    * @param isSorted indicates if records have been sorted
-    * @param sortedColumns
-    */
-   public TableInfo(String tblname, Schema schema, Map<String,Integer> offsets, int recordlen, boolean isSorted, LinkedList<String> sortedColumns) {
       this.tblname   = tblname;
       this.schema    = schema;
       this.offsets   = offsets;
       this.recordlen = recordlen;
-      this.isSorted  = isSorted;
-      this.sortedColumns   = sortedColumns ;
    }
    
    /**
@@ -113,21 +95,11 @@ public class TableInfo {
          return STR_SIZE(schema.length(fldname));
    }
 
-   // CS4432-Project2: getter for isSorted variable
    public boolean isSorted() {
       return isSorted;
    }
 
-   // CS4432-Project2: setter for isSorted variable
    public void setSorted(boolean sorted) {
       isSorted = sorted;
-   }
-
-   public LinkedList<String> getSortedColumns() {
-      return sortedColumns;
-   }
-
-   public void setSortedColumns(LinkedList<String> sortedColumns) {
-      this.sortedColumns = sortedColumns;
    }
 }
